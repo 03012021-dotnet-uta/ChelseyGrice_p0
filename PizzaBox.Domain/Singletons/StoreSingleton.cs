0ed453b769853e2361f2d653d434ff72f777cb9c
fred belotte <fred.belotte@revature.com>
2021-03-04 15:31:43 -0600

    initial commit

---
+++ PizzaBox.Domain/Singletons/StoreSingleton.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using PizzaBox.Domain.Abstracts;
+using PizzaBox.Domain.Models;
+
+namespace PizzaBox.Domain.Singletons
+{
+  public class StoreSingleton
+  {
+    public List<AStore> Stores { get; set; }
+
+    public StoreSingleton()
+    {
+      Stores = new List<AStore>
+      {
+        new FreddyPizza() { Name = "freddy pizza"}
+      };
+    }
+  }
+}
